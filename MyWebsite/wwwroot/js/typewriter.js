461c1d1a9402c809ee235ef4745384755353920c
Lukas98S <Lukasschuetz98@aol.com>
2026-02-09 19:56:22 +0100

    update Style

---
+++ MyWebsite/wwwroot/js/typewriter.js
@@ -0,0 +1,17 @@
+﻿window.typewriterEffect = (elementId, text, speed) => {
+    const element = document.getElementById(elementId);
+
+    if (!element) return;
+
+    element.innerHTML = '';
+    let i = 0;
+
+    function type() {
+        if (i < text.length) {
+            element.innerHTML += text.charAt(i);
+            i++;
+            setTimeout(type, speed);
+        }
+    }
+    type();
+};
